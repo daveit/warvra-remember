689138dd38ef339492aa7e4274a51833c26babb4
“Dave <daveporter@me.com>
2016-11-01 23:47:43 +0800

    contacts editing - first version

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < ApplicationController
+  before_action :set_contact, only: [:edit, :update, :destroy]
 
   def index
     @contacts = Contact.all
@@ -10,7 +11,31 @@ class ContactsController < ApplicationController
     redirect_to village_path(@village)
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @contact.update(contact_params)
+        format.html { redirect_to contacts_url, notice: 'Contact was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @contact.destroy
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: 'Contact was successfully deleted.' }
+    end
+  end
+
   private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
 
   def contact_params
     params.require(:contact).permit(:name, :phone, :mobile, :email, :title_id, :admin, :position_id, :village_id)
